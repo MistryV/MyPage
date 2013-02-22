ccec11befe293c75f0d64052d6aea145d0cf48d1
Mistry <mistry@develop.io>
2013-02-21 22:08:24 -0500

    changed theme

--- wp-content/themes/fotofolio/sidebar.php
+++ wp-content/themes/fotofolio/sidebar.php
@@ -3,10 +3,10 @@
   <?php wp_nav_menu( 'theme_location=main-menu&container=ul&container_class=nav&menu_class=nav sf-menu sf-vertical' ); ?>
   <div class="clear">&nbsp;</div>
   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('main-sidebar') ) : ?>
-  <div class="widgz here">
+<!--   <div class="widgz here">
     <h2><?php _e( 'Categories', WPOP_THEME_SLUG ); ?></h2>		
     <ul><?php wp_list_categories( 'title_li=&show_empty=0&depth=1' ); ?></ul>
-  </div>
+  </div> -->
   <?php endif; ?>
 <?php include( TEMPLATEPATH . '/copyright.php' ); ?>
 </div> <!-- end of sidebar -->
